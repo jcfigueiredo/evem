649d4599a3e76f739a64e82dad78c69bc4c601c5
Claudio Souza <jcfigueiredo@gmail.com>
2023-12-10 10:47:29 -0500

    Removes duplicated tests

--- tests/wildcard.test.ts
+++ tests/wildcard.test.ts
@@ -1,115 +1,56 @@
-import { EvEm } from '~/eventEmitter'; // Adjust the import path as needed
-import { describe, test, expect, beforeEach, vi } from 'vitest';
+import { EvEm } from "~/eventEmitter";
+import { describe, test, expect, beforeEach, vi } from "vitest";
 
-describe('EvEm - Wildcard Subscription Tests', () => {
+describe("EvEm - Wildcard Subscription Tests", () => {
   let emitter: EvEm;
 
   beforeEach(() => {
     emitter = new EvEm();
   });
 
-  test('should receive events for wildcard type subscription (*.type)', () => {
+  test("should receive events for wildcard type subscription (*.type)", () => {
     const wildcardCallback = vi.fn();
 
-    emitter.subscribe('*.type', wildcardCallback);
+    emitter.subscribe("*.type", wildcardCallback);
 
-    const eventData = { data: 'Event Data' };
-    emitter.publish('event.type', eventData);
+    const eventData = { data: "Event Data" };
+    emitter.publish("event.type", eventData);
 
     expect(wildcardCallback).toHaveBeenCalledWith(eventData);
   });
 
-  test('should receive events for wildcard source subscription (source.*)', () => {
+  test("should receive events for wildcard source subscription (source.*)", () => {
     const wildcardCallback = vi.fn();
 
-    emitter.subscribe('source.*', wildcardCallback);
+    emitter.subscribe("source.*", wildcardCallback);
 
-    const eventData = { data: 'Event Data' };
-    emitter.publish('source.event', eventData);
+    const eventData = { data: "Event Data" };
+    emitter.publish("source.event", eventData);
 
     expect(wildcardCallback).toHaveBeenCalledWith(eventData);
   });
 
-  test('should not receive events that do not match the wildcard pattern', () => {
+  test("should not receive events that do not match the wildcard pattern", () => {
     const wildcardCallback = vi.fn();
 
-    emitter.subscribe('unmatched.*', wildcardCallback);
-    emitter.publish('matched.event', { data: 'Test' });
+    emitter.subscribe("unmatched.*", wildcardCallback);
+    emitter.publish("matched.event", { data: "Test" });
 
     expect(wildcardCallback).not.toHaveBeenCalled();
   });
 
-  test('should handle multiple wildcard patterns and combinations', () => {
+  test("should handle multiple wildcard patterns and combinations", () => {
     const wildcardCallback1 = vi.fn();
     const wildcardCallback2 = vi.fn();
 
-    emitter.subscribe('*.event', wildcardCallback1);
-    emitter.subscribe('test.*', wildcardCallback2);
+    emitter.subscribe("*.event", wildcardCallback1);
+    emitter.subscribe("test.*", wildcardCallback2);
 
-    const eventData1 = { type: 'Event1' };
-    const eventData2 = { type: 'Event2' };
+    const eventData1 = { type: "Event1" };
+    const eventData2 = { type: "Event2" };
 
-    emitter.publish('test.event', eventData1);
-    emitter.publish('demo.event', eventData2);
-
-    expect(wildcardCallback1).toHaveBeenCalledWith(eventData1);
-    expect(wildcardCallback1).toHaveBeenCalledWith(eventData2);
-    expect(wildcardCallback2).toHaveBeenCalledWith(eventData1);
-    expect(wildcardCallback2).not.toHaveBeenCalledWith(eventData2);
-  });
-});
-
-
-describe('EvEm - Wildcard Subscription Tests', () => {
-  let emitter: EvEm;
-
-  beforeEach(() => {
-    emitter = new EvEm();
-  });
-
-  test('should receive events for wildcard type subscription (*.type)', () => {
-    const wildcardCallback = vi.fn();
-
-    emitter.subscribe('*.type', wildcardCallback);
-
-    const eventData = { data: 'Event Data' };
-    emitter.publish('event.type', eventData);
-
-    expect(wildcardCallback).toHaveBeenCalledWith(eventData);
-  });
-
-  test('should receive events for wildcard source subscription (source.*)', () => {
-    const wildcardCallback = vi.fn();
-
-    emitter.subscribe('source.*', wildcardCallback);
-
-    const eventData = { data: 'Event Data' };
-    emitter.publish('source.event', eventData);
-
-    expect(wildcardCallback).toHaveBeenCalledWith(eventData);
-  });
-
-  test('should not receive events that do not match the wildcard pattern', () => {
-    const wildcardCallback = vi.fn();
-
-    emitter.subscribe('unmatched.*', wildcardCallback);
-    emitter.publish('matched.event', { data: 'Test' });
-
-    expect(wildcardCallback).not.toHaveBeenCalled();
-  });
-
-  test('should handle multiple wildcard patterns and combinations', () => {
-    const wildcardCallback1 = vi.fn();
-    const wildcardCallback2 = vi.fn();
-
-    emitter.subscribe('*.event', wildcardCallback1);
-    emitter.subscribe('test.*', wildcardCallback2);
-
-    const eventData1 = { type: 'Event1' };
-    const eventData2 = { type: 'Event2' };
-
-    emitter.publish('test.event', eventData1);
-    emitter.publish('demo.event', eventData2);
+    emitter.publish("test.event", eventData1);
+    emitter.publish("demo.event", eventData2);
 
     expect(wildcardCallback1).toHaveBeenCalledWith(eventData1);
     expect(wildcardCallback1).toHaveBeenCalledWith(eventData2);
